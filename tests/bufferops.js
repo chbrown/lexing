ea8d0af59e07a34f506910e9850366dbbe241254
Christopher Brown <io@henrian.com>
2016-03-06 21:42:32 -0600

    Change assert.equal -> strictEqual

--- tests/bufferops.js
+++ tests/bufferops.js
@@ -10,30 +10,30 @@ describe('buffer util', () => {
       assert(compare(haystack, new Buffer('<<')));
     });
     it('should not start with "other"', () => {
-      assert.equal(compare(haystack, new Buffer('other')), false);
+      assert.strictEqual(compare(haystack, new Buffer('other')), false);
     });
   });
 
   describe('#indexOf', () => {
     var haystack = new Buffer('<< /Name (hello) >>');
     it('should have hello at index 0', () => {
-      assert.equal(indexOf(haystack, new Buffer('hello')), 10);
+      assert.strictEqual(indexOf(haystack, new Buffer('hello')), 10);
     });
     it('should not have other at index 0', () => {
-      assert.equal(indexOf(haystack, new Buffer('other')), null);
+      assert.strictEqual(indexOf(haystack, new Buffer('other')), null);
     });
   });
 
   describe('#equalTo', () => {
     var haystack = new Buffer('hello world');
     it('should equal hello at 0:5', () => {
-      assert.equal(equalTo(haystack, new Buffer('hello'), 0, 5), true);
+      assert.strictEqual(equalTo(haystack, new Buffer('hello'), 0, 5), true);
     });
     it('should equal he at 0:2', () => {
-      assert.equal(equalTo(haystack, new Buffer('he'), 0, 2), true);
+      assert.strictEqual(equalTo(haystack, new Buffer('he'), 0, 2), true);
     });
     it('should not equal world at index 0:5', () => {
-      assert.equal(equalTo(haystack, new Buffer('world'), 0, 5), false);
+      assert.strictEqual(equalTo(haystack, new Buffer('world'), 0, 5), false);
     });
   });
 });
